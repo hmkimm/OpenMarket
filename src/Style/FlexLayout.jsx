dbc81a0d34288982fc25d0059ace4556e7570bc9
hmkimm <lisamin7@naver.com>
2023-09-07 02:36:44 +0900

    Design: 공통 디자인 레이아웃 추가

---
+++ src/Style/FlexLayout.jsx
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+const FlexLayout = styled.div`
+  display: flex;
+  justify-content: ${(props) => props.jc || "center"};
+  align-items: ${(props) => props.ai || "center"};
+  gap: ${(props) => props.gap || 0};
+`;
+
+export default FlexLayout;
---
+++ src/Style/Layout.jsx
@@ -0,0 +1,12 @@
+import { styled, css } from "styled-components";
+
+const LayoutStyle = css`
+  width: 1280px;
+  margin: 0 auto;
+  padding-top: 120px;
+`;
+
+const Layout = styled.div`
+  ${LayoutStyle}
+`;
+export { Layout, LayoutStyle };
