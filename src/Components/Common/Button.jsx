87adcac5c322d0004c7eb0bbbf36bb52c92e4b46
hmkimm <lisamin7@naver.com>
2023-09-07 02:33:49 +0900

    Style: 버튼 컴포넌트 배경색상 글로벌 스타일 적용

--- src/Components/Common/Button.jsx
+++ src/Components/Common/Button.jsx
@@ -10,7 +10,7 @@ const ButtonStyle = styled.button`
   display: ${(props) => props.display || "block"};
   padding: ${(props) => props.padding || "19px"};
   margin: ${(props) => props.margin || "36px 0 0 0"};
-  background-color: ${(props) => (props.empty ? "#d5d4d4" : "#21bf48")};
+  background-color: ${(props) => (props.empty ? "var(--gray)" : "var(--primary)")};
   border-radius: 5px;
   font-size: 18px;
   color: #fff;
