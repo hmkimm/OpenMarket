7a0ca7209e2856bda60ac1de20048f115b1c46b5
hmkimm <lisamin7@naver.com>
2023-09-07 19:45:06 +0900

    Fix: props가 DOM으로 전송되는 것처럼 보이는 현상 수정

--- src/Style/FlexLayout.jsx
+++ src/Style/FlexLayout.jsx
@@ -2,9 +2,9 @@ import { styled } from "styled-components";
 
 const FlexLayout = styled.div`
   display: flex;
-  justify-content: ${(props) => props.jc || "center"};
-  align-items: ${(props) => props.ai || "center"};
-  gap: ${(props) => props.gap || 0};
+  justify-content: ${(props) => props.$jc || "center"};
+  align-items: ${(props) => props.$ai || "center"};
+  gap: ${(props) => props.$gap || 0};
 `;
 
 export default FlexLayout;
