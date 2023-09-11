8438f03bc32479e1ae5d8789fbb39d6d7f506458
hmkimm <lisamin7@naver.com>
2023-09-12 01:50:40 +0900

    Refactor: 재사용 타입 지정

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface CartItem {
+  img: string;
+  provider: string;
+  name: string;
+  price: number;
+  shippingMethod: string;
+  shippingFee: number;
+  quantity: number;
+  myCart: number;
+  cartId: number;
+  productId: number;
+}
