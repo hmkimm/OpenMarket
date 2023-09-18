54078993ef2ac52c2b540365045b5f93587b0bf7
hmkimm <lisamin7@naver.com>
2023-09-19 00:10:29 +0900

    Refactor: seller product api 변경

--- src/API/Product/SellerProductListAPI.ts
+++ src/API/Product/SellerProductListAPI.ts
@@ -2,22 +2,25 @@ import URL from "../URL";
 import { useRecoilValue } from "recoil";
 import userToken from "Recoil/userToken/userToken";
 
-const SellerProductListAPI = async () => {
+const SellerProductListAPI = () => {
   const token = useRecoilValue(userToken);
-  try {
-    const res = await fetch(`${URL}/seller/`, {
-      method: "GET",
-      headers: {
-        Authorization: `JWT ${token}`,
-      },
-    });
-    const data = await res.json();
-    console.log("seller product api 통신");
-    return data;
-  } catch (error) {
-    console.error("seller api 오류", error);
-    throw error;
-  }
+  const fetchProduct = async () => {
+    try {
+      const res = await fetch(`${URL}/seller/`, {
+        method: "GET",
+        headers: {
+          Authorization: `JWT ${token}`,
+        },
+      });
+      const data = await res.json();
+      console.log("seller product api 통신");
+      return data;
+    } catch (error) {
+      console.error("seller api 오류", error);
+      throw error;
+    }
+  };
+  return fetchProduct;
 };
 
 export default SellerProductListAPI;
