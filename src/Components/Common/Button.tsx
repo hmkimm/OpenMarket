3705db261d6189c2a4d2fd51313e26cff1444e31
hmkimm <lisamin7@naver.com>
2023-09-21 16:35:21 +0900

    Fix: Button props 수정

--- src/Components/Common/Button.tsx
+++ src/Components/Common/Button.tsx
@@ -1,16 +1,16 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
 import { styled } from "styled-components";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
   width?: string;
   display?: string;
   $padding?: string;
   $margin?: string;
-  empty?: boolean;
+  $empty?: boolean;
 }
 
-const Button = ({ children, ...props }: ButtonProps) => {
-  return <ButtonStyle {...props}>{children}</ButtonStyle>;
+const Button = ({ children, ...rest }: ButtonProps) => {
+  return <ButtonStyle {...rest}>{children}</ButtonStyle>;
 };
 
 const ButtonStyle = styled.button<ButtonProps>`
@@ -19,7 +19,7 @@ const ButtonStyle = styled.button<ButtonProps>`
   padding: ${(props) => props.$padding || "19px"};
   margin: ${(props) => props.$margin || "36px 0 0 0"};
   background-color: ${(props) =>
-    props.empty ? "var(--light-gray)" : "var(--primary)"};
+    props.$empty ? "var(--light-gray)" : "var(--primary)"};
   border-radius: 5px;
   font-size: 18px;
   color: #fff;
