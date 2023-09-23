51bfe3d8f3d9448c68824d9acc5ed9e16271b629
hmkimm <lisamin7@naver.com>
2023-09-24 00:53:33 +0900

    Design: margin 속성 추가

--- src/Style/FlexLayout.tsx
+++ src/Style/FlexLayout.tsx
@@ -5,12 +5,14 @@ interface FlexLayout {
   $jc?: string;
   $ai?: string;
   $gap?: string;
+  $margin?: string;
 }
 const FlexLayout = styled.div<FlexLayout>`
   display: flex;
   justify-content: ${(props) => props.$jc || "center"};
   align-items: ${(props) => props.$ai || "center"};
   gap: ${(props) => props.$gap || 0};
+  margin: ${(props) => props.$margin};
 `;
 
 export default FlexLayout;
