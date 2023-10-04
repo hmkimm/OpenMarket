3d5e0c3779a56c0d9e3b4f61ca5fd5a12d81d81a
hmkimm <lisamin7@naver.com>
2023-10-04 13:12:08 +0900

    Design: css 속성 추가

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -5,17 +5,26 @@ interface InputProps extends React.ComponentPropsWithoutRef<"input"> {
   label: string;
   margin?: string;
   left?: string;
+  display?: string;
+  mb?: string;
+  labelwidth?: string;
 }
 
 interface Div {
   margin?: string;
+  display?: string;
 }
-
+interface LabelType {
+  mb?: string;
+  labelwidth?: string;
+}
 const Input = (props: InputProps) => {
-  const { id, label, type, ...rest } = props;
+  const { id, display, margin, labelwidth, label, type, mb, ...rest } = props;
   return (
-    <InputLayout>
-      <Label htmlFor={id}>{label}</Label>
+    <InputLayout display={display} margin={margin}>
+      <Label htmlFor={id} mb={mb} labelwidth={labelwidth}>
+        {label}
+      </Label>
       <TextInput id={id} type={type || "text"} {...rest} />
     </InputLayout>
   );
@@ -23,18 +32,22 @@ const Input = (props: InputProps) => {
 
 const InputLayout = styled.div<Div>`
   margin: ${(props) => props.margin || "0 0  40px 0"};
-  display: flex;
+  display: ${(props) => props.display || "flex"};
+
   align-items: center;
 `;
-const Label = styled.label`
-  width: 80px;
+const Label = styled.label<LabelType>`
+  width: ${(props) => props.labelwidth || "80px"};
   margin-right: 80px;
+  display: block;
+  margin-bottom: ${(props) => props.mb};
+  color: var(--gray);
 `;
 
 const TextInput = styled.input`
   border: 1px solid var(--light-gray);
   width: ${(props) => props.width || "334px"};
-  height: 40px;
+  height: ${(props) => props.height || "40px"};
   padding: 15px;
 `;
 export default Input;
