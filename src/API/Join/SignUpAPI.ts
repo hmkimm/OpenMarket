4aadc73174dee1fd5fd0098ea3fd2bc2778971d3
hmkimm <lisamin7@naver.com>
2023-10-12 22:58:39 +0900

    Feat: 회원가입 기능 추가

---
+++ src/API/Join/SignUpAPI.ts
@@ -0,0 +1,21 @@
+import React from "react";
+import URL from "API/URL";
+import { RegisterInputsType } from "Pages/BuyerJoin";
+
+const SignUpAPI = (registerInputs: RegisterInputsType) => {
+  const handleSignUp = async () => {
+    const res = await fetch(`${URL}/accounts/signup/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(registerInputs),
+    });
+
+    const data = await res.json();
+    console.log(data);
+  };
+  return handleSignUp;
+};
+
+export default SignUpAPI;
