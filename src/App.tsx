8288547adf3e8a2fcee471c2cbc51a0a9f5e6f01
hmkimm <lisamin7@naver.com>
2023-10-25 13:47:05 +0900

    Feat: App.tsx에 helmet 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import AppRoutes from "./Router/AppRoutes";
 import GlobalStyle from "./GlobalStyle";
 
 import { RecoilRoot } from "recoil";
+import { Helmet } from "react-helmet-async";
 
 const App: React.FC = () => {
   return (
     <RecoilRoot>
       <GlobalStyle />
-      <AppRoutes />
+      <Helmet>
+        <AppRoutes />
+      </Helmet>
     </RecoilRoot>
   );
 };
