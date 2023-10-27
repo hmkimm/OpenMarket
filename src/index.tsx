95b559d3f330e9e8193fcfc1f317cbcaa03db019
hmkimm <lisamin7@naver.com>
2023-10-27 16:54:21 +0900

    Feat: react-snap 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,25 @@ import { HelmetProvider } from "react-helmet-async";
 const container = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(container);
 
-if (container.hasChildNodes()) {
-  ReactDOM.hydrateRoot(
-    container,
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  );
-} else {
-  root.render(
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  );
-}
+// if (container.hasChildNodes()) {
+//   ReactDOM.hydrateRoot(
+//     container,
+//     <HelmetProvider>
+//       <App />
+//     </HelmetProvider>
+//   );
+// } else {
+//   root.render(
+//     <HelmetProvider>
+//       <App />
+//     </HelmetProvider>
+//   );
+// }
 
+// const container = document.getElementById("root") as Element | DocumentFragment;
+// const root = createRoot(container);
+root.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
